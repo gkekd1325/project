21be3c168570195adaafaa8eabf89d69b1713ddb
gkekd1325 <gkekd1325@naver.com>
2022-07-19 17:30:38 +0900

    jimin

--- js/script.js
+++ js/script.js
@@ -4,11 +4,24 @@ function manage(){
     location.href="page1.html";
     
 }
+var logout = document.querySelector(".logout");
+logout.addEventListener("click", out);
+function out(){
+    location.href="index.html";
+    
+}
 
 var menu3 = document.querySelector(".menu3");
 menu3.addEventListener("click", daily);
 function daily(){
     location.href="page3.html";
+    
+}
+
+var popup = document.querySelector(".open");
+popup.addEventListener("click", pop);
+function pop(){
+    window.open("popup.html","_parent");
     
 }
 

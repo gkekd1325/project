7502e92615f48d231422b5f9d42af187c8aeb7ad
hwan <snepid@gmail.com>
2022-07-19 17:18:39 +0900

    rename
    filerename

---
+++ js/load.js
@@ -0,0 +1,10 @@
+$('.calendarload').load('calendar.html');
+
+$(".click_pop").click(function(e){
+  e.preventDefault();
+  var target = $(this).attr("href");
+  
+  $("#clientEditorModal .modal-content").load(target, function() {
+    $("#clientEditorModal").modal("fadeIn");
+  });
+});

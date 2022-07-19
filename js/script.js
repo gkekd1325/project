529818b3267bd3c5098fa42aba4a23374d278d83
gkekd1325 <gkekd1325@naver.com>
2022-07-19 11:48:55 +0900

    jimin jimin

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var menu1 = document.querySelector(".menu1");
+menu1.addEventListener("click", manage);
+function manage(){
+    location.href="page1.html";
+    
+}
+
+var menu3 = document.querySelector(".menu3");
+menu3.addEventListener("click", daily);
+function daily(){
+    location.href="page3.html";
+    
+}
+

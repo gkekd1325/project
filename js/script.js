eaeac0a2cbd265ddd09cb16b074616dc90a685a6
hwan <snepid@gmail.com>
2022-07-22 11:00:04 +0900

    page unite

--- js/script.js
+++ js/script.js
@@ -1,27 +1,71 @@
-var menu1 = document.querySelector(".menu1");
-menu1.addEventListener("click", manage);
-function manage(){
-    location.href="page1.html";
+// var menu1 = document.querySelector(".menu1");
+// menu1.addEventListener("click", manage);
+// function manage(){
+//     location.href="page1.html";
     
-}
-var logout = document.querySelector(".logout");
-logout.addEventListener("click", out);
-function out(){
-    location.href="index.html";
+// }
+// var menu3 = document.querySelector(".menu3");
+// menu3.addEventListener("click", daily);
+// function daily(){
+//     location.href="page3.html";
     
-}
+// }
+// $(function(){
+//     $(".menu2").click(function(){
+//         location.href="page2.html";
+//     });
+// });
 
-var menu3 = document.querySelector(".menu3");
-menu3.addEventListener("click", daily);
-function daily(){
-    location.href="page3.html";
-    
-}
 
-// var popup = document.querySelector(".open");
-// popup.addEventListener("click", pop);
-// function pop(){
-//     window.open("modal1.html","_parent");
+$(function(){
+    $(".menu1").click(function(){
+        $('.right').load('list.html');
+    });
+});
+
+$(function(){
+    $(".menu2").click(function(){
+        $('.right').load('calendarload.html');
+    });
+});
+
+$(function(){
+    $(".menu3").click(function(){
+        $('.right').load('list.html');
+    });
+});
+
+$(document).ready(function () {
+    $("nav li").each(function () {
+        $(this).click(function () {
+            $(this).addClass("on");
+            $(this).siblings().removeClass("on");
+        });
+    });
+});
+
+
+// var logout = document.querySelector(".logout");
+// logout.addEventListener("click", out);
+// function out(){
+//     location.href="index.html";
     
 // }
 
+$(function(){
+    $(".logout").click(function(){
+        location.href="index.html";
+    });
+});
+$(function(){
+    $(".info3").click(function(){
+        location.href="index.html";
+    });
+});
+  
+var popup = document.querySelector(".open");
+popup.addEventListener("click", pop);
+function pop(){
+    window.open("modal1.html","_parent");
+    
+}
